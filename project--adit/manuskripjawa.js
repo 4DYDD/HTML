da77f6cc1c715e427bbde27893e180a93a82ccb4
4dydd <haidiraditya16@gmail.com>
2023-08-26 15:36:42 +0800

    nambahi anuu... samting

--- project--adit/manuskripjawa.js
+++ project--adit/manuskripjawa.js
@@ -2,6 +2,7 @@
 let audio = document.getElementById("musik");
 let gambarguweh = document.getElementById("gambarguweh");
 let samting = document.getElementById("samting");
+
 let vh = window.innerHeight / 100;
 
 audio.volume = 0;
@@ -17,7 +18,10 @@ window.addEventListener("scroll", function () {
     if (top <= 50 * vh) {
         // memutar musik jika belum dimainkan
 
-        gambarguweh.style.border = "1px solid black";
+        gambarguweh.style.borderRadius = "0%";
+        gambarguweh.style.boxShadow = "0px 0px 1px 1px yellow, 0px 0px 5px 2px rgb(255, 255, 255)";
+        gambarguweh.style.opacity = "1";
+        gambarguweh.style.filter = "brightness(1)";
 
         if (audio.paused) {
 
@@ -37,7 +41,10 @@ window.addEventListener("scroll", function () {
 
     } else {
 
-        gambarguweh.style.border = "0px solid black";
+        gambarguweh.style.borderRadius = "50%";
+        gambarguweh.style.boxShadow = "";
+        gambarguweh.style.opacity = "0";
+        gambarguweh.style.filter = "brightness(5)";
 
         if (!audio.paused) {
 
